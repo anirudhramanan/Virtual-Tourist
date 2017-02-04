4b5bb4e38007b22e6880af8107dcd30713d7e8c0
anirudh.r <anirudh.r@flipkart.com>
2017-02-05 00:38:08 +0530

    added tap and hold gesture for adding pin annotations to the map view

--- Virtual Tourist/MapViewController.swift
+++ Virtual Tourist/MapViewController.swift
@@ -7,11 +7,48 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var gestureRecognizer: UIGestureRecognizer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        configureDelegate()
+    }
+    
+    @IBAction func addPinToMap(_ gestureRecognizer: UIGestureRecognizer) {
+        self.gestureRecognizer  = gestureRecognizer
+        
+        /* This is done to receive only one tap and hold gesture at a time. If the state of the gesture has begun, we remove
+         the gesture recognizer from the map view, and it is only add back when the annotation gets added on the mapview.
+         This way we can make sure that only one pin annotation can be added in a single tap and hold gesture.
+         */
+        
+        if gestureRecognizer.state == UIGestureRecognizerState.began {
+            mapView.removeGestureRecognizer(gestureRecognizer)
+        }
+        
+        let pinPoint: CGPoint = gestureRecognizer.location(in: mapView)
+        let locCoord: CLLocationCoordinate2D = mapView.convert(pinPoint, toCoordinateFrom: mapView)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locCoord
+        self.mapView.addAnnotation(annotation)
+    }
+}
+
+extension MapViewController : MKMapViewDelegate {
+    
+    func configureDelegate () {
+        mapView.delegate = self
+    }
+    
+    func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
+        /*
+         Since the annotation pin has been added, enable the gesture recognizer to add another pin
+         */
+        mapView.addGestureRecognizer(gestureRecognizer)
     }
 }
